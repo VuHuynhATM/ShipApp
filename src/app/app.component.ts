de0c4ba5365bc6605aa201cd16cf78a5f20963f8
VuATM <vuhtse140902@gmail.com>
2023-04-01 15:03:49 +0700

    ship url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -297,7 +297,7 @@ export class AppComponent {
                 Authorization: 'my-auth-token'
             })
         };
-        this.http.post('https://localhost:7026/api/Ship', this.request, httpOptions).toPromise().then(data => {
+        this.http.post('https://esmpfree-001-site1.etempurl.com/api/Ship', this.request, httpOptions).toPromise().then(data => {
             console.log(data);
             this.getDataService();
             this.hideDialogservice();
